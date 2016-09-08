b7d9b7fc8c25d57c8fe101b974b14c41d0d0c437
Phil Howard <phil@gadgetoid.com>
2016-09-08 09:51:32 +0100

    Initial Commit

---
+++ library/motephat.py
@@ -0,0 +1,150 @@
+import atexit
+import RPi.GPIO as GPIO
+
+
+DAT = 10
+CLK = 11
+NUM_PIXELS_PER_CHANNEL = 16
+NUM_CHANNELS = 4
+BRIGHTNESS = 7
+
+CHANNELS = [8,7,25,24]
+
+pixels = [
+    [[0,0,0,BRIGHTNESS]] * NUM_PIXELS_PER_CHANNEL,
+    [[0,0,0,BRIGHTNESS]] * NUM_PIXELS_PER_CHANNEL,
+    [[0,0,0,BRIGHTNESS]] * NUM_PIXELS_PER_CHANNEL,
+    [[0,0,0,BRIGHTNESS]] * NUM_PIXELS_PER_CHANNEL
+]
+
+_gpio_setup = False
+_clear_on_exit = True
+
+def _exit():
+    if _clear_on_exit:
+        clear()
+        show()
+    GPIO.cleanup()
+
+def set_brightness(brightness):
+    """Set the brightness of all pixels
+
+    :param brightness: Brightness: 0.0 to 1.0
+    """
+    for c in range(NUM_CHANNELS):
+        for x in range(NUM_PIXELS_PER_CHANNEL):
+            pixels[c][x][3] = int(31.0 * brightness) & 0b11111
+
+def clear_channel(c):
+    """Clear a single channel
+
+    :param c: Channel to clear: 0 to 3
+    """
+    for x in range(NUM_PIXELS_PER_CHANNEL):
+        pixels[c][x][0:3] = [0,0,0]
+
+def clear():
+    """Clear the pixel buffer"""
+    for c in range(NUM_CHANNELS):
+        for x in range(NUM_PIXELS_PER_CHANNEL):
+            pixels[c][x][0:3] = [0,0,0]
+
+def _select_channel(c):
+    for x in range(NUM_CHANNELS):
+        GPIO.output(CHANNELS[x], GPIO.LOW if x==c else GPIO.HIGH)
+
+def _write_byte(byte):
+    for x in range(8):
+        GPIO.output(DAT, byte & 0b10000000)
+        GPIO.output(CLK, 1)
+        byte <<= 1
+        GPIO.output(CLK, 0)
+
+# Emit exactly enough clock pulses to latch the small dark die APA102s which are weird
+# for some reason it takes 36 clocks, the other IC takes just 4 (number of pixels/2)
+def _eof():
+    GPIO.output(DAT, 0)
+    for x in range(36):
+        GPIO.output(CLK, 1)
+        GPIO.output(CLK, 0)
+
+def _sof():
+    GPIO.output(DAT,0)
+    for x in range(32):
+        GPIO.output(CLK, 1)
+        GPIO.output(CLK, 0)
+
+def show():
+    """Output the buffer to Mote pHAT"""
+    global _gpio_setup
+
+    if not _gpio_setup:
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+        GPIO.setup([DAT,CLK],GPIO.OUT)
+        GPIO.setup(CHANNELS,GPIO.OUT)
+        _gpio_setup = True
+
+    for index, channel in enumerate(pixels):
+        _select_channel(index)
+        _sof()
+        for pixel in channel:
+            r, g, b, brightness = pixel
+            _write_byte(0b11100000 | brightness)
+            _write_byte(b)
+            _write_byte(g)
+            _write_byte(r)
+
+        _eof()
+
+def set_all(r, g, b, brightness=None):
+    """Set the RGB value and optionally brightness of all pixels
+
+    If you don't supply a brightness value, the last value set for each pixel be kept.
+
+    :param r: Amount of red: 0 to 255
+    :param g: Amount of green: 0 to 255
+    :param b: Amount of blue: 0 to 255
+    :param brightness: Brightness: 0.0 to 1.0 (default around 0.2)
+    """
+    for c in range(NUM_CHANNELS):
+        for x in range(NUM_PIXELS_PER_CHANNEL):
+            set_pixel(c, x, r, g, b, brightness)
+
+def set_pixel(c, x, r, g, b, brightness=None):
+    """Set the RGB value, and optionally brightness, of a single pixel
+    
+    If you don't supply a brightness value, the last value will be kept.
+
+    :param x: The horizontal position of the pixel: 0 to 7
+    :param r: Amount of red: 0 to 255
+    :param g: Amount of green: 0 to 255
+    :param b: Amount of blue: 0 to 255
+    :param brightness: Brightness: 0.0 to 1.0 (default around 0.2)
+    """
+    c %= NUM_CHANNELS
+    x %= NUM_PIXELS_PER_CHANNEL
+
+    if brightness is None:
+        brightness = pixels[c][x][3]
+    else:
+        brightness = int(31.0 * brightness) & 0b11111
+
+    pixels[c][x] = [int(r) & 0xff,int(g) & 0xff,int(b) & 0xff,brightness]
+
+def set_clear_on_exit(value=True):
+    """Set whether Mote pHAT should be cleared upon exit
+
+    By default Mote pHAT will turn off the pixels on exit, but calling::
+
+        blinkt.set_clear_on_exit(False)
+
+    Will ensure that it does not.
+
+    :param value: True or False (default True)
+    """
+    global _clear_on_exit
+    _clear_on_exit = value
+
+atexit.register(_exit)
+
---
+++ library/test.py
@@ -0,0 +1,18 @@
+import colorsys
+
+import motephat
+
+offset = 0
+
+while True:
+    offset += 1
+    for channel in range(4):
+        for pixel in range(16):
+            hue = offset + (10 * (channel * 16) + pixel)
+            hue %= 360
+            hue /= 360.0
+
+            r, g, b = [int(c*255) for c in colorsys.hsv_to_rgb(hue, 1.0, 1.0)]
+            motephat.set_pixel(channel, pixel, r, g, b)
+
+    motephat.show()
